643852c78131b3525c3399a05eb05926e2eb9b51
jyapujuju <danieldangol@gmail.com>
2017-03-16 17:25:49 +0545

    validation

--- app/controllers/instructor/courses_controller.rb
+++ app/controllers/instructor/courses_controller.rb
@@ -1,12 +1,22 @@
 class Instructor::CourseController < ApplicationController
   before_action :authenticate_user!
+
   def new
     @course = Course.new
   end
+
   def create
     @courses = current_user.courses.create(course_params)
-    redirect_to instructor_course_path(@course)
+    if
+      redirect_to instructor_course_path(@course)
+    else
+      render :new, status: :unprocessable_entity
+    end
 
+  end
+
+  def show
+    @course = Course.find(params[:id])
   end
 
   private
